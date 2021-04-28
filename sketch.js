26ae7ae0ff9ea4f4afe3250a5124f17cc69f50ac
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-04-28 16:11:30 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+
+let state ='title';
+
+function setup() {
+  createCanvas(600, 600);  
+  
+}
+
+function draw() {
+  
+  if (state === 'title');{
+  title();
+  } else (state === 'level 1'){
+    level1();
+  }
+}
+
+function title(){  
+background(252, 23, 3);
+  textSize(100);
+  stroke(200);
+  text('JAK 4', 150, 150);
+}
+
+function level1(){
+  background(252, 244, 3);
+}
