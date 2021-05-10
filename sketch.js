4b1e7827066ddbde443e6b2eb068996517908257
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-05-09 18:54:48 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,24 @@
 'use strict'
 
 
-
 let state ='title';
 let cnv;
-let points = 5;
+let points = 0;
+let w = 900;
+let h = 900;
+let character;
+let rocks = [];
+
 
 function setup(){
-  cnv = createCanvas(600,600);
+  cnv = createCanvas(w,h);
+  text('Move Controlls')
    textFont ('Times New Roman');
+  character = new Character();
+  rocks [10] = new Rocks();
+
 }
+
 function draw(){
   if (state === 'title'){
     title();
@@ -20,49 +29,100 @@ function draw(){
   } else if (state === 'level 1') {
       level1();
     cnv.mouseClicked(function(){
-        console.log('level1');
+        console.log('level1MousedClicked');
         state = 'level 1';
       });
   } else {
     youWon();
-    cnv.mouseClicked('canvas You Won')
+    cnv.mouseClicked('youWonMouseClicked');
     state = 'You Won';
   }
 }
+
+function keyPressed(){
+ if(keyCode == LEFT_ARROW){
+  character.direction = 'left'
+ }else if (keyCode == RIGHT_ARROW){
+  character.direction = 'right'
+ }else if (keyCode == UP_ARROW){
+   character.direction = 'up'
+ }else if (keyCode == DOWN_ARROW){
+ character.direction = 'down'
+ }
+
+}
+
 function canvasClicked(){
   console.log('canvas is clicked');
 }
+
 function mousedPressed(){
   state = 'level 1';
 
 }
+
 function title(){
-  background(659, 37, 133);
-  textSize(50);
-  text('Ryans Game', 10,100);
+  background(6188, 235, 229); 
+  
+  textSize(20);
+  text('MOVE CONTROLS: W,A,S,D', 90,90);
+  
+  textSize(100);
+  text('Shoot at Anything', 90, 250);
+  
+  textSize(40);
+  text('Start', 400,height - 400);
+  
+  textSize(20);
+  fill(0, 51, 255);
+  text('Red Circle is minus points, Black cricle and Green Sqaure are one point', 100,300);
   stroke(255);
 
-  textSize(20);
-  text('click me anywhere to play', 200,500);
 }
+
 function level1(){
-  background(195, 245, 157);
-  textSize(20);
-  text('shoot for points', 250, height - 60);
-  textSize(50);
+  background(0, 68, 140);
+  fill(208, 212, 21);
+
+if (random(1) <=0.02){
+  rocks.push(new Rocks());
+}  
+  
+  textSize(90);
   text('LETS GO:Shoot', 150,100);
+  character.display();
+  character.move;
+  
+ 
+  for (let i= 10; i < rocks.length ; i++){
+  rocks[i].display();
+  rocks[i].move();
+  }
+  
+  for (let i = rocks.length - 2; i >=0; i--){
+  if (dist(character.x,character.y, rocks[10].x, rocks[10].y) <=(character.r + rocks[10].r)/3){
+    points++;
+    console.log(points);
+    rocks.splice(i, 2);
+  }
+  }
+  fill (250, 245, 172);
+  textSize(67);
+  text('points' + points, 555, 850);
 }
+
 function level1MouseClicked(){
   points ++;
 
   console.log('canvas is clicked on level 1');
-  console.log(' =' + points);
+  console.log('points =' + points);
 
-  if (points >= 5){
+  if (points <= 40){
     state = 'YOU WON'
   }
 
 }
+
 function youWon(){
    background(252, 186, 3);
   textSize(50);
