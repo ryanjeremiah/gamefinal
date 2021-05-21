314c64dadbfb570ba9389fb222e34f70d23a783d
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-05-20 22:24:36 -0700

    Update character.js

--- character.js
+++ character.js
@@ -1,26 +1,33 @@
 class Character{
-  constructor(){
-    this.r = 55;
-    this.x = w/10;
+  constructor(x, y, r){
+    this.r = 150;
+    this.x = w/5;
     this.y = h - this.r;
-    this.speed= 1;
+    this.speed= 5;
     this.direction = 'still';
+   
   }
   display(){
-    fill(245, 66, 206);
-    ellipse(this.x, this.y, this.r, this.r);
+     image(characterImg, this.x, this.y, this.r, this.r);
   }
   
   move(){
-    switch (this.dirction){
-      case 'right':
-        break;
-        this.x += this.speed;
-      case 'left':
-        this.x -= this.speed;
-        break;
+    switch (this.direction){
       case 'still':
+        break; 
+      case 'right':
+    if (this.x < w -this.r /5){ 
+        this.x += this.speed;
+    }
         break;
+      case 'left':
+    if (this.x < w - this.r /5 > 0){ 
+        this.x -= this.speed;
+    }
+        break;
+      default:
+        break;
+        
     }
   }
 }
